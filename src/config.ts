1fe65e9f1972749e43cce2e0e766c659e26dd9f0
dystopian <lamineabdellah03@gmail.com>
2024-06-24 01:01:08 +0100

    moved the nigaglitch to /public and made chanages to config.ts

--- src/config.ts
+++ src/config.ts
@@ -43,7 +43,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: '/src/assets/images/nigaglitch.jpg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: '/nigaglitch.jpg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Abdellah Lamine',
   bio: 'your average cyber security student,long way to gooo.',
   links: [
